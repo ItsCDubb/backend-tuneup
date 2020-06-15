518636207ee7368b096bcc60a198039722c07953
Chris Warren <chris@chriswarren.dev>
2020-06-14 21:38:28 -0400

    Initial Commit

--- tuneup.py
+++ tuneup.py
@@ -5,20 +5,33 @@
 Use the timeit and cProfile libraries to find bad code.
 """
 
-__author__ = "???"
+__author__ = "Chris Warren"
 
 import cProfile
 import pstats
 import functools
+import timeit
 
 
 def profile(func):
     """A cProfile decorator function that can be used to
     measure performance.
     """
-    # Be sure to review the lesson material on decorators.
-    # You need to understand how they are constructed and used.
-    raise NotImplementedError("Complete this decorator function")
+    @functools.wraps(func)
+    def profile_wrapper(*args, **kwargs):
+        performance_object = cProfile.Profile()
+        performance_object.enable()
+        result = func(*args, **kwargs)
+        performance_object.disable
+
+        get_stats_obj = pstats.Stats(performance_object)
+        get_stats_obj.strip_dirs()
+        get_stats_obj.sort_stats('cumulative')
+        get_stats_obj.print_stats()
+
+        return result
+
+    return profile_wrapper
 
 
 def read_movies(src):
@@ -35,22 +48,26 @@ def is_duplicate(title, movies):
             return True
     return False
 
+@profile
 
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list."""
+    movie_list = {}
     movies = read_movies(src)
-    duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
-    return duplicates
+    for movie in movies:
+        if movie_list.get(movie):
+            movie_list[movie] += 1
+        else:
+            movie_list[movie] = 1
+    
+    return [k for k, in movie_list.items() if v > 1]
 
 
 def timeit_helper():
     """Part A: Obtain some profiling measurements using timeit."""
-    # YOUR CODE GOES HERE
-    pass
+    t = timeit.Timer(stmt="main()", setup="from __main__ import main")
+    results = min(t.repeat(repeat=7, number=5)) / 5
+    print("Best time across 7 repeats of 5 runs per repeat " + str(results) + " sec")
 
 
 def main():
